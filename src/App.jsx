4ba25adf5a40a4aa374de8e6d958ef0d5b2583be
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-07-06 22:30:38 +0100

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS dependencies to package.json
    - Configured Tailwind CSS in index.css
    - Updated App component to include React Router for navigation
    - Created Dashboard and Test components
    - Set up BrowserRouter in main.jsx for routing support
    - Added basic styling to Test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Test from "./Components/Test";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
-
-  return (
-    <>
-      h
-    </>
-  )
+  return <div>
+    {/* <Test /> */}
+    <Routes>
+      <Route path="/Dashboard" element={<Dashboard />}/>
+    </Routes>
+  </div>;
 }
 
-export default App
+export default App;
---
+++ src/Components/Test.jsx
@@ -0,0 +1,10 @@
+
+function Test() {
+  return (
+    <div className="bg-amber-300 border-4 border-blue-700 w-30 m-4 text-red-600">
+        Text
+    </div>
+  )
+}
+
+export default Test
