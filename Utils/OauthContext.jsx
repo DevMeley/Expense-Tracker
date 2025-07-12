a63ca297ef7ed74599df72ee9efe4cdc68e840e7
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-07-12 22:33:57 +0100

    Created .env file to store Oauth keys. created firebase.js that handle initialization of firebase. Created OauthContext

---
+++ Utils/OauthContext.jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [idToken, setIdToken] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const token = await user.getIdToken();
+        setIdToken(token);
+        setUser(user);
+      } else {
+        setUser(null);
+        setIdToken(null);
+      }
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+
+  return (
+    <AuthContext.Provider value={{user, idToken, loading}}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
---
+++ firebase.js
@@ -0,0 +1,24 @@
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STOREGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+googleProvider.addScope('profile');
+googleProvider.addScope('email');
+googleProvider.addScope("profilePhoto")
